3f11636b3b236011e07fa0459e2c6b980da75ad4
Maxim <sogrinms@gmail.com>
2022-01-04 17:53:03 +0300

    initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header>
+			<div className="max-width">
+
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+	return (
+		<section className='home'>
+			<div className='page__inner'></div>
+		</section>
+	);
+};
+
+export default Home;
